ec249e35d8a2fc7ec7b529d0cef82e50804fbfaa
waytoabv <127306391+waytoabv@users.noreply.github.com>
2024-03-19 22:12:43 +0100

    Delete sketchybar directory

--- sketchybar/items/volumealias.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sketchybar --add alias "Kontrollzentrum,Sound" right                      \
-           --rename "Kontrollzentrum,Sound" volume_alias                  \
-           --set volume_alias icon.drawing=on                             \
-                              alias.scale=0.9                            \
-                              alias.color=$GREY                           \
-                              alias.update_freq=5
--- sketchybar/plugins/date.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-sketchybar --set $NAME label="$(LC_TIME="de_DE.UTF-8" date '+%a %d. %b')" 
