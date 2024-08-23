371964fe6b883198ec49f5fc2de4fdce9935c3da
waytoabv <lbock@mailbox.org>
2024-08-23 23:00:55 +0200

    fish stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,9 @@ abbr --add cat bat
 abbr --add cd z
 abbr --add c clear
 abbr --add rm trash
+abbr --add .. prevd
+abbr --add gitl _fzf_search_git_log
+abbr --add gits _fzf_search_git_status
 
 # starship
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
